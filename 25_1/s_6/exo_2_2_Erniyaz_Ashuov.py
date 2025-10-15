6aa4f501c6162a2af4278f8a7d65c7ef1aea5a6f
ErniyazCode <151911924+ErniyazCode@users.noreply.github.com>
2025-10-16 01:05:19 +0500

    Create exo_2_2_Erniyaz_Ashuov.py

---
+++ 25_1/s_6/exo_2_2_Erniyaz_Ashuov.py
@@ -0,0 +1,17 @@
+#  Problem 2 (Analyse Sorting Algorithms)
+def analyze_bubble_sort():
+    return "time complexity O(n^2)"
+
+def analyze_quick_sort():
+    return "time complexity O(n log n)"
+
+def analyze_merge_sort():
+    return "time complexity O(n log n)"
+
+def analyze_heap_sort():
+    return "time complexity O(n log n)"
+
+print(analyze_bubble_sort())
+print(analyze_quick_sort())
+print(analyze_merge_sort())
+print(analyze_heap_sort())
