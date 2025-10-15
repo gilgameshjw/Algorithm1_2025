49809f2c919218cf3eec68cd909be0eff80a83a5
ErniyazCode <151911924+ErniyazCode@users.noreply.github.com>
2025-10-16 00:56:59 +0500

    Create exo_2_1_1_Erniyaz_Ashuov.py

---
+++ 25_1/s_6/exo_2_1_1_Erniyaz_Ashuov.py
@@ -0,0 +1,10 @@
+#problem1 - write own bad O(n2) sorting
+def bubble_sort(arr):
+    for i in range(len(arr)):
+        for j in range(0, len(arr)-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+arr = [64, 34, 25, 12, 22, 11, 90]
+print(bubble_sort(arr))
